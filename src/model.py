623c3ec90e679f4cd1534b26d3647761cf4351d3
MichelDeudon <michel.deudon@hotmail.com>
2020-05-15 00:04:27 +0200

    edited model architecture

--- src/model.py
+++ src/model.py
@@ -15,7 +15,7 @@ class Model(nn.Module):
             hidden_dim: int, number of hidden channels
             kernel_size: int, kernel size
             pool_size: int, pool size (chunk). Default 10 pixels (1 ha = 100m x 100m).
-            n_max: int, maximum number of boats per chunks. Default 2.
+            n_max: int, maximum number of boats per chunks. Default 1.
             pad: bool, padding to keep input shape. Necessary for Residual Block (TODO).
             device: str, pytorch device
         '''
@@ -24,15 +24,15 @@ class Model(nn.Module):
         self.folder = 'i{}_h{}_k{}_p{}_n{},'.format(input_dim, hidden_dim, kernel_size, pool_size, n_max)
         self.device = torch.device(device if torch.cuda.is_available() else 'cpu')
 
-        self.block1 = nn.Sequential(
+        self.embed = nn.Sequential(
             nn.Conv2d(in_channels=input_dim, out_channels=hidden_dim, kernel_size=kernel_size, padding=int(pad)*kernel_size//2),
             nn.PReLU(),
             nn.BatchNorm2d(hidden_dim),
         )
-        self.block1[0].weight = torch.nn.init.orthogonal_(self.block1[0].weight)
+        self.embed[0].weight = torch.nn.init.orthogonal_(self.embed[0].weight)
         #self.block1[0].bias = torch.nn.init.constant_(self.layer0[0].bias, -0.1)
         
-        self.block2 = nn.Sequential(
+        self.residual = nn.Sequential(
             nn.Conv2d(in_channels=hidden_dim, out_channels=hidden_dim, kernel_size=3, padding=1),
             nn.PReLU(),
             nn.Conv2d(in_channels=hidden_dim, out_channels=hidden_dim, kernel_size=3, padding=1),
@@ -40,8 +40,9 @@ class Model(nn.Module):
         )
         
         self.max_pool = nn.MaxPool2d(pool_size, stride=pool_size)
-        
-        self.block3 = nn.Sequential(
+                
+        self.encode_patch = nn.Sequential(
+            nn.MaxPool2d(pool_size, stride=pool_size),
             nn.Conv2d(in_channels=hidden_dim, out_channels=hidden_dim, kernel_size=1, padding=0),
             nn.PReLU(),
             nn.Conv2d(in_channels=hidden_dim, out_channels=n_max+1, kernel_size=1, padding=0),
@@ -51,26 +52,31 @@ class Model(nn.Module):
         self.to(self.device)
         
         
-    def forward(self, x):
+    def forward(self, x, ndwi_mask=False, water_ndwi=0.4):
         '''
         Predict boat presence (or counts) in an image x
         Args:
             x: tensor (B, C_in, H, W), aerial images
         Returns:
-            x: tensor (B, C_h, H, W), hidden tensor
-            y_hat: tensor (B, 1), boat presence (or counts)
+            pixel_embedding: tensor (B, C_h, H, W), hidden tensor
+            density_map: tensor (B, 1, H//pool_size, W//pool_size), boat density
+            p_hat: tensor (B, 1), proba boat presence
+            y_hat: tensor (B, 1), boat counts (expectation)
         '''
         
         x = x.to(self.device)
         batch_size, channels, height, width = x.shape 
-        pixel_embedding = self.block1(x) 
-        pixel_embedding = pixel_embedding + self.block2(pixel_embedding) # h1 (B, C_h, H, W)
-        chunk_encoding = self.max_pool(pixel_embedding) # h2 (B, C_h, H//pool_size, W//pool_size)
-        z = self.block3(chunk_encoding) # z (B, n_max+1, H//pool_size, W//pool_size) multinomial distribution
-        p_hat = torch.sum(z[:,1:], dim=1, keepdim=True) # (B, 1, H//pool_size, W//pool_size) # probability there is a boat or more
-        p_hat = torch.max(torch.max(p_hat,dim=-1).values ,dim=-1).values # tight lower bound on probability there is a boat in image
-        density_map = torch.sum(torch.cat([float(k)*z[:,k:k+1] for k in range(z.size(1))],1), dim=1, keepdim=True) # density map (counts)
-        y_hat = torch.sum(density_map, (2,3))*(p_hat>0.5) # estimate number of boats in image (where there are boats)
+        pixel_embedding = self.embed(x) 
+        pixel_embedding = pixel_embedding + self.residual(pixel_embedding) # h1 (B, C_h, H, W)
+        z = self.encode_patch(pixel_embedding) # z (B, n_max+1, H//pool_size, W//pool_size) multinomial distribution Z_i=k if k boats, 0 <= k < n_max+1
+        p_hat = torch.sum(z[:,1:], dim=1, keepdim=True) # probability there is a boat or more (for each chunk)
+        p_hat = torch.max(torch.max(p_hat,dim=-1).values ,dim=-1).values # (B, 1) tight lower bound on probability there is a boat in full image
+        density_map = torch.sum(torch.cat([float(k)*z[:,k:k+1] for k in range(z.size(1))],1), dim=1, keepdim=True) # density map (counts), resolution: pool_size (10 pix = 100 m)
+        #if ndwi_mask is True:
+        #    water_mask = 1.0*(x[:,1:2]<0.5*(-water_ndwi+1)) # background, negative, rescaled ndwi >> mask density_heatmap # (B, 1, H, W),
+        #    proba_water = self.max_pool(water_mask)
+        #    density_map = density_map*proba_water
+        y_hat = torch.sum(density_map, (2,3)) #*(p_hat>0.5) # estimate number of boats in image (where there are boats) ###### remove threshold
         return pixel_embedding, density_map, p_hat, y_hat
     
     def get_loss(self, x, y, n=None, ld=0.0, metric='BCE', water_ndwi=0.5):
@@ -90,26 +96,19 @@ class Model(nn.Module):
         
         x = x.to(self.device)
         x_hidden, density_map, p_hat, y_hat = self.forward(x)  # (B,1,n_filters,H,W)
-        
+                        
+        # loss for boat presence (proba vs. binary)
         p = 1.0*(y>0)
-
-        criterion = torch.nn.SmoothL1Loss(reduction='mean')
-        reg_error = criterion(y_hat, y)
-        
-        if n is None:
-            criterion = torch.nn.BCELoss(reduction='mean') ##### change to BCEWithLogitsLoss
-            clf_error = criterion(p_hat, p)
-        else:
-            criterion = torch.nn.BCELoss(reduction='none') ##### change to BCEWithLogitsLoss
-            clf_error = torch.sum(criterion(p_hat, p)/n)
-                
-        ##### water_mask = 1.0*(x[:,1:2]<0.5*(water_ndwi+1)) # background, negative, rescaled ndwi >> mask density_heatmap
-        #loss = clf_error + 0.001*reg_error #+ ld*torch.mean(x_hidden*(1.0-water_mask))
-        #if metric=='BCE':
-        #    loss = clf_error
-        #else:
-        loss = clf_error + 0.5*reg_error #+ ld*torch.mean(x_hidden*(1.0-water_mask))
+        criterion = torch.nn.BCELoss(reduction='mean') ##### change to BCEWithLogitsLoss
+        clf_error = criterion(p_hat, p)
             
+        # loss for boat counts (expected vs. label)
+        criterion = torch.nn.SmoothL1Loss(reduction='mean') 
+        reg_error = criterion(y_hat, y)
+                
+        ##### downsanple water_wask by pool_size
+        #noise ld*torch.mean(density_map*(1.0-water_mask))
+        loss = clf_error + 0.5*reg_error #+ ld*torch.mean(x_hidden*(1.0-water_mask))
         return clf_error, reg_error, loss
     
     def load_checkpoint(self, checkpoint_file):
@@ -144,16 +143,21 @@ class Model(nn.Module):
         
         for idx in range(n_frames):
             chunked_tensor = []
-            for i in range(n_rows):
-                for j in range(n_cols):
-                    chunked_tensor.append(x[idx:idx+1,:,i*chunk_size:(i+1)*chunk_size,j*chunk_size:(j+1)*chunk_size])
-            chunked_tensor = torch.cat(chunked_tensor, 0) # (P, C_in, chunk_size, chunk_size)
-            _, density_map, p_hat, y_hat = self.forward(chunked_tensor.float())
-            p_hat = p_hat.detach().cpu().numpy()
-            p_hat = p_hat>boat_threshold # binarize prediction
-            p_hat = p_hat.reshape(n_rows, n_cols)
-
-            heatmaps.append(p_hat)
-            counts.append(p_hat.sum())
-    
+            #for i in range(n_rows):
+            #    for j in range(n_cols):
+            #        chunked_tensor.append(x[idx:idx+1,:,i*chunk_size:(i+1)*chunk_size,j*chunk_size:(j+1)*chunk_size])
+            #chunked_tensor = torch.cat(chunked_tensor, 0) # (P, C_in, chunk_size, chunk_size)
+            #_, density_map, p_hat, y_hat = self.forward(chunked_tensor.float())
+            #p_hat = p_hat.detach().cpu().numpy()
+            #p_hat = p_hat>boat_threshold # binarize prediction
+            #p_hat = p_hat.reshape(n_rows, n_cols)
+            
+            #heatmaps.append(p_hat)
+            #counts.append(p_hat.sum())
+            
+            _, density_map, p_hat, y_hat = self.forward(x[idx:idx+1].float())
+            heatmaps.append(density_map[0,0].detach().cpu().numpy()) # density_map
+            counts.append(float(y_hat.detach().cpu().numpy()))
+            
+        
         return heatmaps, counts
